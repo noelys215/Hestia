f8b72806d30180aa7da0dfb04e3b4a4c18cb68ee
Henry B <noelys215@gmail.com>
2023-03-29 21:55:57 -0400

    add query for ads on home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
 import { useAuth } from '../context/auth';
+import axios from 'axios';
 
 export const Home = () => {
+	/* Context */
 	// eslint-disable-next-line no-unused-vars
 	const [auth, setAuth] = useAuth();
+	/* State */
+	const [adsForSale, setAdsForSale] = useState();
+	const [adsForRent, setAdsForRent] = useState();
+
+	useEffect(() => {
+		fetchAds();
+	}, []);
+
+	const fetchAds = async () => {
+		try {
+			const { data } = axios.get('/ads');
+			setAdsForSale(data?.adsForSale);
+			setAdsForRent(data?.adsForRent);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	return (
 		<div>
