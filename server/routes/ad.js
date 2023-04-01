6e15cc5e0f76fec88aaaffa5c443c1f003aa7864
Henry B <noelys215@gmail.com>
2023-03-31 20:35:09 -0400

    ad backend route to fetch single ad

--- server/routes/ad.js
+++ server/routes/ad.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { uploadImage, removeImage, create, ads } from '../controllers/ad.js';
+import { uploadImage, removeImage, create, ads, read } from '../controllers/ad.js';
 import { requireSignIn } from '../middlewares/auth.js';
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post('/upload-image', requireSignIn, uploadImage);
 router.post('/remove-image', requireSignIn, removeImage);
 router.post('/ad', requireSignIn, create);
 router.get('/ads', requireSignIn, ads);
+router.get('/ad/:slug', read);
 
 export default router;
